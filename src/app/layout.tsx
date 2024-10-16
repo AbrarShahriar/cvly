31f59aefdd1d1a52fe93070f1bbce0b3c33dd63f
AbrarShahriar <AbrarShahriar321@gmail.com>
2024-10-17 00:34:43 +0600

    integrated beam analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import NextTopLoader from "nextjs-toploader";
+import Head from "next/head";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -22,6 +24,13 @@ export default function RootLayout({
   return (
     <html lang="en" className={montserrat.className}>
       <body className={`antialiased`}>
+        <Script
+          src="https://beamanalytics.b-cdn.net/beam.min.js"
+          data-token={process.env.BEAM_ANALYTICS_TOKEN as string}
+          async
+          id="important-script"
+          strategy="beforeInteractive"
+        ></Script>
         <NextTopLoader showSpinner={false} />
         {children}
       </body>
